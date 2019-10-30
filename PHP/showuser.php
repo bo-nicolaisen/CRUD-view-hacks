ac48604002ec3182968f4d86fc481325fd6bee81
Bo Nicolajsen <squirrelworks@users.noreply.github.com>
2019-10-30 08:41:17 +0100

    link og div

--- PHP/showuser.php
+++ PHP/showuser.php
@@ -1,3 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>CRUD view Hack</title>
+    <meta name="description" content="DESCRIPTION">
+   <link rel="stylesheet" href="../CSS/site.css">
+
+    <!--[if lt IE 9]>
+      <script src = "http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+<h1>Edit user</h1>
 <?php
 
 include "db.php";
@@ -12,3 +28,8 @@ echo "<table><tr>";
 echo "<th>".$user['username']."</th>"."<th>" .$user['email']."</th>"." <th>".$user['password']."</th>";
 echo "</tr></table>";
  ?>
+
+
+  </body>
+
+  </html>
--- index.php
+++ index.php
@@ -5,7 +5,7 @@
      <meta charset="UTF-8">
      <title>CRUD view Hack</title>
      <meta name="description" content="DESCRIPTION">
-    <link rel="stylesheet" href="PATH">
+    <link rel="stylesheet" href="CSS/site.css">
 
      <!--[if lt IE 9]>
        <script src = "http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
@@ -13,7 +13,7 @@
  </head>
 
  <body>
-
+<h1>User management module</h1>
 
 <?php
 include "PHP/db.php";
@@ -29,7 +29,8 @@ if($data){
 echo "<table>";
 foreach ($data as $row) {
 echo "<tr>";
-echo "<th>".$row['username']."</th>"."<th>" .$row['email']."</th>"." <th>".$row['password']."</th>"."<th><a href='PHP/showuser.php?usereID=".$row['id']."'>show user</a></th>";
+echo "<th>".$row['username']."</th>"."<th>" .$row['email']."</th>"." <th>".$row['password'].
+"</th>"."<th><a href='PHP/showuser.php?usereID=".$row['id']."'>Edit</a></th>";
 echo "</tr>";
 }
 echo "</table>";
@@ -45,24 +46,3 @@ else {
  </body>
 
  </html>
-
-<!--
-<table>
-  <tr>
-    <th>Firstname</th>
-    <th>Lastname</th>
-    <th>Age</th>
-  </tr>
-  <tr>
-    <td>Jill</td>
-    <td>Smith</td>
-    <td>50</td>
-  </tr>
-  <tr>
-    <td>Eve</td>
-    <td>Jackson</td>
-    <td>94</td>
-  </tr>
-</table>
-*/
--->
